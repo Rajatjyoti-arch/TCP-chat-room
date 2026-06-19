b866212408fad666c01be343a41ed23a601803a3
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-20 00:12:39 +0530

    Refined client.py

--- client.py
+++ client.py
@@ -24,4 +24,4 @@ def write():
         message = f'{alias}: {input("")}'
         client.send(message.encode('ascii'))
 
-mesage_thread = threading.
+mesage_thread = threading.Thread(target=recieve)
